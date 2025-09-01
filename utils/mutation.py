43bb608fc045e5e780feac4c547802837cd184bf
Aayush Sharma <168636020+Aayush-09-bit@users.noreply.github.com>
2025-09-01 12:30:22 +0530

    Delete utils directory

--- utils/mutation.py
+++
@@ -1,4 +0,0 @@
-def mutate_sequence(sequence: str, position: int, new_res: str) -> str:
-    if position < 1 or position > len(sequence):
-        raise ValueError("Position out of range")
-    return sequence[:position - 1] + new_res + sequence[position:]
